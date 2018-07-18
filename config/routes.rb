6f3737ac50da564153cff88c5717e164f2681a8c
José Raimundo <j.zmais@gmail.com>
2018-07-18 12:42:52 +0000

    Organização das paginas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'autorias/autorias'
   resources :projects
   resources :events
   resources :books
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,27 @@
 
 Person.create!(
     nome: "José Raimundo Barbosa",
-    email: "j.zmais@gmail.com",
+    email: "jsraimundob@gmail.com",
     titulacao: "Graduando",
-    lattes: "www.lattes.com.br",
+    lattes: "http://lattes.cnpq.br/1119257001769205",
     funcao: "Pesquisador",
     imagem: "jose.jpg"
+)
+
+Person.create!(
+    nome: "Carlos Danilo Miranda Regis",
+    email: "regis.danilo@gmail.com",
+    titulacao: "Doutor",
+    lattes: "http://lattes.cnpq.br/3729525547666162",
+    funcao: "Coordenador",
+    imagem: "danilo.jpg"
+)
+
+Project.create!(
+    titulo: "Serviço de Testes Automatizados para Codificadores HEVC (HM) Utilizando Processamento Paralelo e Distribuído",
+    descricao: " Este projeto propõe o desenvolvimento de um serviço de testes automatizados de codificadores baseados em HM. Para possibilitar a redução do tempo de testes, será elaborada uma arquitetura utilizando processamento paralelo e distribuído na qual várias instâncias do codificador poderão ser executadas simultaneamente. Para calcular a eficiência do codificador e a diferença de desempenho entre os codificadores, será acoplado ao sistema um conjunto de métricas objetivas.",
+    edital: "2018/1 Interconecta",
+    datainicio: "04/2018"
 )
 
 Event.create!(
